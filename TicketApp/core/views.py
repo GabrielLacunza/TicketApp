cdc4da892ce1e0416c7c9fcd1489bd0fbb0f0a19
cratilox <102319363+cratilox@users.noreply.github.com>
2023-10-06 16:19:01 -0300

    06/10 Completo sin diseño
    Login + Consulta tickets + Crud Ticket administrador

--- TicketApp/core/views.py
+++ TicketApp/core/views.py
@@ -1,7 +1,54 @@
-from django.shortcuts import render
-
-# Create your views here.
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import logout
+from django.contrib import messages
 
 def home(request):
     return render(request, "home.html", {})
+
+from .forms import CustomUserCreationForm  # Importa tu formulario personalizado
+
+def registro(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)  # Usa tu formulario personalizado
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = User.objects.create_user(username=username, password=password)
+            user.save()
+            return redirect('login') 
+    else:
+        form = CustomUserCreationForm()  # Usa tu formulario personalizado
+    
+    return render(request, 'registro.html', {'form': form})
+
+def iniciar_sesion(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            remember_me = request.POST.get('remember_me')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                if not remember_me:
+                    request.session.set_expiry(0)
+                return redirect('home')
+            else:
+                messages.error(request, 'Cuenta inexistente o contraseña incorrecta.')
+        else:
+            messages.error(request, 'Error en el formulario. Verifica tus credenciales.')
+
+    else:
+        form = AuthenticationForm()
+    
+    return render(request, 'login.html', {'form': form})
+
+def cerrar_sesion(request):
+    logout(request)
+    return redirect('home')
+
