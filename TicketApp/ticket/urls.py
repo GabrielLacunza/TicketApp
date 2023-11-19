1eb41cd970bf43a836548dd1ac85ba8a280c2e3f
cratilox <ig.ralph@duocuc.cl>
2023-11-19 04:33:54 -0300

    Commit 5 requerimientos listos
    Se completaron los 2 siguientes requerimientos:
    - Reserva de tickets
    - Listado de tickets reservados por el Usuario
    
    Usuarios a utilizar estan en escritos en el template login.html

--- TicketApp/ticket/urls.py
+++ TicketApp/ticket/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-from .views import ticket_detalle, reserva_ticket
+from .views import ticket_detalle, reserva_ticket, ticket_list
 
 
 urlpatterns = [
@@ -11,11 +11,11 @@ urlpatterns = [
 
     path('add_ticket', views.TicketCreate.as_view(), name="add_ticket"),
     
-    path('edit_ticket/<int:pk>', views.TicketUpdate.as_view(), name='edit_ticket'),
+    path('edit_ticket/<uuid:pk>', views.TicketUpdate.as_view(), name='edit_ticket'),
     
-    path('list_ticket', views.TicketList.as_view(), name="list_ticket"),
+    path('list_ticket', ticket_list, name="list_ticket"),
     
-    path('del_ticket/<int:pk>', views.TicketDelete.as_view(), name="del_ticket"),
+    path('del_ticket/<uuid:pk>', views.TicketDelete.as_view(), name="del_ticket"),
     
 
     # ==== Funciones para el reservado de tickets
